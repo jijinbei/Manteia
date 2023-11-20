7dacc6924ad49d5805d2467532b12ff0823fe7ab
jijinbei <hikoharu14142@outlook.jp>
2023-11-20 21:54:29 +0900

    HelpCogの微調整

--- cogs/HelpCog.py
+++ cogs/HelpCog.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord import Embed, File
+from lib.embed import create_embed
 
 class HelpCog(commands.Cog):
     def __init__(self, bot):
@@ -9,21 +9,15 @@ class HelpCog(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{self.bot.user.name}のHelpCogが起動しました。')
-
-    # ヘルプ機能
-    @commands.command()
-    async def help(self, message):
-        if message.author.bot:
-            return
-        else:
-            embed = Embed(title="ヘルプ機能", description="コマンドの説明。最初に『!』をつけてください。", color=0xff9300)
-            embed.add_field(name="!save", value="添付ファイルから画像の保存をする ex)!save 量子力学2 2022 + 添付ファイル", inline=False)
-            embed.add_field(name="!Support", value="管理人にサポートメッセージを送る(DM)", inline=False)
-            embed.add_field(name="!cleanup (※管理人のみ)", value="テキストチャンネルのメッセージをすべて消す", inline=False)
-            fname="fig.jpeg" # アップロードするときのファイル名 自由に決めて良いですが、拡張子を忘れないように
-            file = File(fp="fig.jpeg",filename=fname,spoiler=False) # ローカル画像からFileオブジェクトを作成
-            embed.set_image(url=f"attachment://{fname}") # embedに画像を埋め込むときのURLはattachment://ファイル名
-            await message.channel.send(file=file, embed=embed) # ファイルとembedを両方添えて送信する
-
+    
+    @commands.command(name='help', help='ヘルプを表示')
+    async def help(self, ctx):
+        help_embed = create_embed("ヘルプ", "試験問題を検索するためのBotです。")
+        help_embed.add_field(name="!save", value="画像を保存します。!save (授業名) (年度) + 画像添付\n使用例) !save 統計力学 2003 + 統計力学のテストを添付", inline=False)
+        help_embed.add_field(name="!exams", value="指定されたコースの全ての年度の試験問題を取得します。\n使用例) !exams 統計力学", inline=False)
+        help_embed.add_field(name="!list", value="利用可能な試験と年度の一覧を表示します。", inline=False)
+        help_embed.add_field(name="!help", value="ヘルプを表示します。", inline=False)
+        await ctx.send(embed=help_embed)
+        
 async def setup(bot):
     await bot.add_cog(HelpCog(bot))
